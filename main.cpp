d5b486d8c11f9857b25be72943972508a2e2381e
fengyun17 <389755043@qq.com>
2024-10-20 19:14:48 +0800

    第9次修改提交了

--- main.cpp
+++ main.cpp
@@ -11,6 +11,40 @@ struct word {
     std::map<std::string, std::map<std::string, std::map<std::string, desc>>> data;
 };
 
+template <typename T> struct node{
+    bool leaf;
+    node<T> *parent;
+    node<T> *skip;
+    T data;
+    std::deque<node<T>*> chs;
+    explicit node(T&& d=nullptr){
+        data = std::move(d);
+        leaf = false;
+        parent = nullptr;
+        skip = nullptr;
+    }
+};
+
+template <typename T> class ToT{
+protected:
+    int cnt;
+    bool (*eq)(T, T);
+    bool (*com)(T, T);
+    node<T> *root;
+    node<T> *hot;
+public:
+    ToT(bool (*e)(T, T), bool (*c)(T, T)) : cnt(0), hot(nullptr) {
+        eq = e;
+        com = c;
+        root = new node<T>;
+    }
+    ~ToT(){
+        delete root; root = nullptr;
+    }
+    [[nodiscard]] int size() const {return cnt;}
+    [[nodiscard]] bool empty() const {return !root ? false : true;}
+};
+
 int main() {
     std::cout << "Hello, World! yes" << std::endl;
     const char *s = "test";
